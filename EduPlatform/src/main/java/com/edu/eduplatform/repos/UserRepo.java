a326f242b1f79070560fae1a9e0470182ef357fb
Fatma Ahmed <113565704+FatmaAhmed25@users.noreply.github.com>
2024-06-18 07:05:40 +0300

    Users creation from excel sheet & manually

--- EduPlatform/src/main/java/com/edu/eduplatform/repos/UserRepo.java
+++ EduPlatform/src/main/java/com/edu/eduplatform/repos/UserRepo.java
@@ -9,5 +9,6 @@ public interface UserRepo extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmail(String username);
+    boolean existsByEmail(String email);
 
 }
