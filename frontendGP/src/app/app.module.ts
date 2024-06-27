0c24e5ad773d648c521788f66b72fb393512c48f
HanaYehia1 <hana.ayman@softylus.com>
2024-06-28 00:20:30 +0300

    login frontend

---
+++ frontendGP/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoursesStudentComponent } from './courses-student/courses-student.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesStudentComponent,
+    HeaderComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
