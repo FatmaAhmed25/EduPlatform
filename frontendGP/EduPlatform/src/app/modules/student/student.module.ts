bd083af5a47efafe1228c6ac9db5c483c499a422
sarahyasser <sarahyasserxo@gmail.com>
2024-07-01 03:23:25 +0300

    create-quiz

--- frontendGP/EduPlatform/src/app/modules/student/student.module.ts
+++ frontendGP/EduPlatform/src/app/modules/student/student.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { StudentCoursesComponent } from './student-courses/student-courses.component';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 @NgModule({
   declarations: [
     StudentDashboardComponent,
     QuizComponent,
-    StudentCoursesComponent
+    StudentCoursesComponent,
+    
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatProgressSpinnerModule,
   ],
   exports: [StudentDashboardComponent,
     StudentCoursesComponent]
---
+++ frontendGP/EduPlatform/src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatProgressSpinnerModule,
+  ],
+  exports: [
+    MatProgressSpinnerModule,
+  ]
+})
+export class SharedModule { }
