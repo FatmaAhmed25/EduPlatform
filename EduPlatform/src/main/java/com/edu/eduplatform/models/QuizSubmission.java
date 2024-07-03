b48272898c5e702fbac451edfa02d41047f254f8
Fatma Ahmed <113565704+FatmaAhmed25@users.noreply.github.com>
2024-07-03 12:46:20 +0300

    Auto Grading for essay quizzes

--- EduPlatform/src/main/java/com/edu/eduplatform/models/QuizSubmission.java
+++ EduPlatform/src/main/java/com/edu/eduplatform/models/QuizSubmission.java
@@ -28,7 +28,7 @@ public class QuizSubmission
     private Quiz quiz;
 
     @Column(name = "submissionTime", nullable = false)
-    private LocalDateTime submissionTime;
+    private LocalDateTime submissionTime=LocalDateTime.now();
 
     @Column(name = "totalGrade")
     private double totalGrade;
